acaa25e478d14ae25129f16922abedb3d39467a2
Brandon Mitchell <brandon@thewholedoubt.com>
2020-04-26 11:52:57 -0700

    Enable non-panicking asserts.

--- src/assert.h
+++ src/assert.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <portable/macros.h>
+#include <portable/stdbool.h>
 
 BEGIN_DECLS
 
@@ -11,22 +12,27 @@ void stacktrace(int skip);
  * Wrappers for defining custom assert based on whether the macro
  * ASSERT_PANIC was defined at the moment ASSERT was called.
  */
-#ifdef ASSERT_PANIC
+#ifdef ASSERTS
+#    ifdef ASSERT_PANIC
+#        define ASSERT(_x) do {                                     \
+                if (!(_x)) {                                        \
+                    _assert(#_x, __FILE__, __LINE__, true);         \
+                }                                                   \
+            } while (0)
 
-#define ASSERT(_x) do {                                 \
-        if (!(_x)) {                                    \
-            _assert(#_x, __FILE__, __LINE__, true);     \
-        }                                               \
-    } while (0)
+#        define NOT_REACHED() ASSERT(0)
+#    else /* !ASSERT_PANIC */
+#        define ASSERT(_x) do {                                  \
+                if (!(_x)) {                                     \
+                    _assert(#_x, __FILE__, __LINE__, false);     \
+                }                                                \
+            } while (0)
 
-#define NOT_REACHED() ASSERT(0)
-
-#else
-
-#define ASSERT(_x)
-
-#define NOT_REACHED()
-
-#endif
+#        define NOT_REACHED() ASSERT(0)
+#    endif /* ASSERT_PANIC */
+#else /* !ASSERTS */
+#    define ASSERT(_x)
+#    define NOT_REACHED()
+#endif /* ASSERTS */
 
 END_DECLS
