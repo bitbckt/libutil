51920702a08a91de0fdb482c7f3d6a4ad5e125ee
Brandon Mitchell <brandon@thewholedoubt.com>
2020-05-15 13:38:34 -0700

    Rephrase xwrite() commentary for clarity.

--- src/xwrite.h
+++ src/xwrite.h
@@ -22,10 +22,10 @@
 
 BEGIN_DECLS
 
-/*
- * Like the non-x versions of the same function, but keep writing until either
- * the write is not making progress or there's a real error.  Handle partial
- * writes and EINTR/EAGAIN errors.
+/**
+ * Like write(2), but keep writing until either the write is not
+ * making progress or there's a real error. Handle partial writes and
+ * EINTR/EAGAIN errors.
  */
 ssize_t xwrite(int fd, const void *buffer, size_t size)
     __attribute__((__nonnull__, warn_unused_result));
